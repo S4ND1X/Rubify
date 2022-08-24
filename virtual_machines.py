cd2d2c8ec11a61c1cc2d543f5b9734c7ca29270a
Jorge Sanchez Diaz <sandix390@gmail.com>
2022-08-24 15:24:46 -0500

    hotfix virtual machines provisoning .py

--- virtual_machines.py
+++ virtual_machines.py
@@ -1,75 +1,67 @@
-import json
-import requests
-from flask import (
-    current_app,
-    Blueprint,
-    request,
-    flash,
-    redirect,
-    url_for,
-    render_template,
-)
+from flask import (Blueprint, current_app, request, flash,
+                   redirect, url_for, render_template)
 
 import pulumi
+import pulumi_aws as aws
 import pulumi.automation as auto
-from pulumi_aws import s3
+import os
+from pathlib import Path
 
-bp = Blueprint("sites", __name__, url_prefix="/sites")
+bp = Blueprint("virtual_machines", __name__, url_prefix="/vms")
+instance_types = ['c5.xlarge', 'p2.xlarge', 'p3.2xlarge']
 
-def create_pulumi_program(content: str):
-    # Create a bucket and expose a website index document
-    site_bucket = s3.Bucket(
-        "s3-website-bucket", website=s3.BucketWebsiteArgs(index_document="index.html")
-    )
-    index_content = content
 
-    # Write our index.html into the site bucket
-    s3.BucketObject(
-        "index",
-        bucket=site_bucket.id,
-        content=index_content,
-        key="index.html",
-        content_type="text/html; charset=utf-8",
-    )
+def create_pulumi_program(keydata: str, instance_type=str):
+    # Choose the latest minimal amzn2 Linux AMI.
+    # TODO: Make this something the user can choose.
+    ami = aws.ec2.get_ami(most_recent=True,
+                          owners=["amazon"],
+                          filters=[aws.GetAmiFilterArgs(name="name", values=["*amzn2-ami-minimal-hvm*"])])
 
-    # Set the access policy for the bucket so all objects are readable
-    s3.BucketPolicy(
-        "bucket-policy",
-        bucket=site_bucket.id,
-        policy=site_bucket.id.apply(
-            lambda id: json.dumps(
-                {
-                    "Version": "2012-10-17",
-                    "Statement": {
-                        "Effect": "Allow",
-                        "Principal": "*",
-                        "Action": ["s3:GetObject"],
-                        # Policy refers to bucket explicitly
-                        "Resource": [f"arn:aws:s3:::{id}/*"],
-                    },
-                }
-            )
-        ),
-    )
+    group = aws.ec2.SecurityGroup('web-secgrp',
+                                  description='Enable SSH access',
+                                  ingress=[aws.ec2.SecurityGroupIngressArgs(
+                                      protocol='tcp',
+                                      from_port=22,
+                                      to_port=22,
+                                      cidr_blocks=['0.0.0.0/0'],
+                                  )])
 
-    # Export the website URL
-    pulumi.export("website_url", site_bucket.website_endpoint)
-    pulumi.export("website_content", index_content)
+    public_key = keydata
+    if public_key is None or public_key == "":
+        home = str(Path.home())
+        f = open(os.path.join(home, '.ssh/id_rsa.pub'), 'r')
+        public_key = f.read()
+        f.close()
+
+    public_key = public_key.strip()
+
+    print(f"Public Key: '{public_key}'\n")
+
+    keypair = aws.ec2.KeyPair("dlami-keypair", public_key=public_key)
+
+    server = aws.ec2.Instance('dlami-server',
+                              instance_type=instance_type,
+                              vpc_security_group_ids=[group.id],
+                              key_name=keypair.id,
+                              ami=ami.id)
+
+    pulumi.export('instance_type', server.instance_type)
+    pulumi.export('public_key', keypair.public_key)
+    pulumi.export('public_ip', server.public_ip)
+    pulumi.export('public_dns', server.public_dns)
+
 
 @bp.route("/new", methods=["GET", "POST"])
-def create_site():
-    """creates new sites"""
+def create_vm():
+    """creates new VM"""
     if request.method == "POST":
-        stack_name = request.form.get("site-id")
-        file_url = request.form.get("file-url")
-        if file_url:
-            site_content = requests.get(file_url).text
-        else:
-            site_content = request.form.get("site-content")
+        stack_name = request.form.get("vm-id")
+        keydata = request.form.get("vm-keypair")
+        instance_type = request.form.get("instance_type")
 
         def pulumi_program():
-            return create_pulumi_program(str(site_content))
-
+            return create_pulumi_program(keydata, instance_type)
         try:
             # create a new stack, generating our pulumi program on the fly from the POST body
             stack = auto.create_stack(
@@ -81,21 +73,22 @@ def create_site():
             # deploy the stack, tailing the logs to stdout
             stack.up(on_output=print)
             flash(
-                f"Successfully created site '{stack_name}'", category="success")
+                f"Successfully created VM '{stack_name}'", category="success")
         except auto.StackAlreadyExistsError:
             flash(
-                f"Error: Site with name '{stack_name}' already exists, pick a unique name",
+                f"Error: VM with name '{stack_name}' already exists, pick a unique name",
                 category="danger",
             )
+        return redirect(url_for("virtual_machines.list_vms"))
 
-        return redirect(url_for("sites.list_sites"))
+    current_app.logger.info(f"Instance types: {instance_types}")
+    return render_template("virtual_machines/create.html", instance_types=instance_types, curr_instance_type=None)
 
-    return render_template("sites/create.html")
 
 @bp.route("/", methods=["GET"])
-def list_sites():
-    """lists all sites"""
-    sites = []
+def list_vms():
+    """lists all vms"""
+    vms = []
     org_name = current_app.config["PULUMI_ORG"]
     project_name = current_app.config["PROJECT_NAME"]
     try:
@@ -112,35 +105,34 @@ def list_sites():
                 program=lambda: None,
             )
             outs = stack.outputs()
-            if 'website_url' in outs:
-                sites.append(
+            if 'public_dns' in outs:
+                vms.append(
                     {
                         "name": stack.name,
-                        "url": f"http://{outs['website_url'].value}",
+                        "dns_name": f"{outs['public_dns'].value}",
                         "console_url": f"https://app.pulumi.com/{org_name}/{project_name}/{stack.name}",
                     }
                 )
     except Exception as exn:
         flash(str(exn), category="danger")
 
-    return render_template("sites/index.html", sites=sites)
+    current_app.logger.info(f"VMS: {vms}")
+    return render_template("virtual_machines/index.html", vms=vms)
+
 
 @bp.route("/<string:id>/update", methods=["GET", "POST"])
-def update_site(id: str):
+def update_vm(id: str):
     stack_name = id
-
     if request.method == "POST":
-        file_url = request.form.get("file-url")
-        if file_url:
-            site_content = requests.get(file_url).text
-        else:
-            site_content = str(request.form.get("site-content"))
+        current_app.logger.info(
+            f"Updating VM: {stack_name}, form data: {request.form}")
+        keydata = request.form.get("vm-keypair")
+        current_app.logger.info(f"updating keydata: {keydata}")
+        instance_type = request.form.get("instance_type")
 
+        def pulumi_program():
+            return create_pulumi_program(keydata, instance_type)
         try:
-
-            def pulumi_program():
-                create_pulumi_program(str(site_content))
-
             stack = auto.select_stack(
                 stack_name=stack_name,
                 project_name=current_app.config["PROJECT_NAME"],
@@ -149,16 +141,16 @@ def update_site(id: str):
             stack.set_config("aws:region", auto.ConfigValue("us-east-1"))
             # deploy the stack, tailing the logs to stdout
             stack.up(on_output=print)
-            flash(f"Site '{stack_name}' successfully updated!",
+            flash(f"VM '{stack_name}' successfully updated!",
                   category="success")
         except auto.ConcurrentUpdateError:
             flash(
-                f"Error: site '{stack_name}' already has an update in progress",
+                f"Error: VM '{stack_name}' already has an update in progress",
                 category="danger",
             )
         except Exception as exn:
             flash(str(exn), category="danger")
-        return redirect(url_for("sites.list_sites"))
+        return redirect(url_for("virtual_machines.list_vms"))
 
     stack = auto.select_stack(
         stack_name=stack_name,
@@ -167,12 +159,14 @@ def update_site(id: str):
         program=lambda: None,
     )
     outs = stack.outputs()
-    content_output = outs.get("website_content")
-    content = content_output.value if content_output else None
-    return render_template("sites/update.html", name=stack_name, content=content)
+    public_key = outs.get("public_key")
+    pk = public_key.value if public_key else None
+    instance_type = outs.get("instance_type")
+    return render_template("virtual_machines/update.html", name=stack_name, public_key=pk, instance_types=instance_types, curr_instance_type=instance_type.value)
+
 
 @bp.route("/<string:id>/delete", methods=["POST"])
-def delete_site(id: str):
+def delete_vm(id: str):
     stack_name = id
     try:
         stack = auto.select_stack(
@@ -183,13 +177,13 @@ def delete_site(id: str):
         )
         stack.destroy(on_output=print)
         stack.workspace.remove_stack(stack_name)
-        flash(f"Site '{stack_name}' successfully deleted!", category="success")
+        flash(f"VM '{stack_name}' successfully deleted!", category="success")
     except auto.ConcurrentUpdateError:
         flash(
-            f"Error: Site '{stack_name}' already has update in progress",
+            f"Error: VM '{stack_name}' already has update in progress",
             category="danger",
         )
     except Exception as exn:
         flash(str(exn), category="danger")
 
-    return redirect(url_for("sites.list_sites"))
+    return redirect(url_for("virtual_machines.list_vms"))
