9989669b806a53bca8c0d30fb84a6a24ff4fa5a7
Jorge Sanchez Diaz <sandix390@gmail.com>
2022-08-24 15:06:10 -0500

    sites crud for provisioning done

--- sites.py
+++ sites.py
@@ -1,4 +1,6 @@
+from inspect import stack
 import json
+import site
 import requests
 from flask import (
     current_app,
@@ -58,3 +60,146 @@ def create_pulumi_program(content: str):
     # Export the website URL
     pulumi.export("website_url", site_bucket.website_endpoint)
     pulumi.export("website_content", index_content)
+
+
+@bp.route("/new", methods=["GET", "POST"])
+def create_site():
+    """
+    Create a new website based on the content the user provided
+    """
+    if request.method == "POST":
+        stack_name = request.form.get("site-id")
+        file_url = request.form.get("file-url")
+        if file_url:
+            site_content = requests.get(file_url).text
+        else:
+            site_content = request.form.get("site-content")
+        
+        def pulumi_program():
+            return create_pulumi_program(str(site_content))
+
+        try:
+            # create a new stack, generating our plumi program on the fly from the post body
+            stack = auto.create_stack(
+                stack_name=str(stack_name),
+                project_name=current_app.config["PROJECT_NAME"],
+                program=pulumi_program,
+            )
+            stack.set_config("aws:region", auto.ConfigValue("us-east-1"))
+             # deploy the stack, tailing the logs to stdout
+            stack.up(on_output=print)
+
+            flash(
+                f"Successfully created site '{stack_name}'", 
+                category="success"
+            )
+        except auto.StackAlreadyExistsError:
+            flash(
+                f"Error: Site with name '{stack_name}' already exists, pick a unique name", 
+                category="danger"
+            )
+
+        return redirect(url_for("sites.list_sites"))
+
+    return render_template("sites/create.html")
+
+
+@bp.route("/", methods=["GET"])
+def list_sites():
+    """
+    List all the sites that have been created
+    """
+    sites = []
+    org_name = current_app.config["PULUMI_ORG"]
+    project_name = current_app.config["PROJECT_NAME"]
+    try:
+        ws = auto.LocalWorkspace(
+            project_settings=auto.ProjectSettings(
+                name=project_name, runtime="python")
+        )
+        all_stacks = ws.list_stacks()
+        for stack in all_stacks:
+            stack = auto.select_stack(
+                stack_name=stack.name,
+                project_name=project_name,
+                # no-op program, just to get ouputs
+                program=lambda: None,
+            )
+            outs = stack.outputs()
+            if 'website_url' in outs:
+                sites.append(
+                    {
+                        "name": stack.name,
+                        "url": f"https://{outs['website_url'].value}",
+                        "content": f"https://app.pulumi.com/{org_name}/{project_name}/{stack.name}",
+                    }
+                )
+    except Exception as exn:
+        flash(str(exn), category="danger")
+    
+    return render_template("sites/index.html", sites=sites)
+
+
+@bp.route("/<string:id>/update", methods=["GET", "POST"])
+def update_site(id: str):
+    stack_name = id
+
+    if request.method == "POST":
+        file_url = request.form.get("file-url")
+        if file_url:
+            site_content = requests.get(file_url).text
+        else:
+            site_content = str(request.form.get("site-content") )  
+
+        try:
+            def pulumi_program():
+                return create_pulumi_program(str(site_content)) 
+            
+            stack = auto.select_stack(
+                stack_name=stack_name,
+                project_name=current_app.config["PROJECT_NAME"],
+                program=pulumi_program,
+            )
+            stack.set_config("aws:region", auto.ConfigValue("us-east-1"))
+            # deploy the stack, tailing the logs to stdout
+            stack.up(on_output=print)
+            flash(f"Site '{stack_name}' successfully updated", category="success")
+        
+        except auto.ConcurrentUpdateError:
+            flash(f"Error: Site '{stack_name}' is currently updating", category="danger")
+    
+        except Exception as exn:
+            flash(str(exn), category="danger")
+        return redirect(url_for("sites.list_sites"))
+    
+    stack = auto.select_stack(
+        stack_name=stack_name,
+        project_name=current_app.config["PROJECT_NAME"],
+        # no-op program, just to get ouputs
+        program=lambda: None,
+    )
+    outs = stack.outputs()
+    content_output = outs.get("website_content")
+    content = content_output.value if content_output else None
+    return render_template("sites/update.html", site=stack_name, content=content)
+
+@bp.route("/<string:id>/delete", methods=["POST"])
+def delete_site(id: str):
+    stack_name = id
+
+    try:
+        stack = auto.select_stack(
+            stack_name=stack_name,
+            project_name=current_app.config["PROJECT_NAME"],
+            # no-op program, just to get ouputs
+            program=lambda: None,
+        )
+        stack.destroy(on_output=print)
+        stack.workspace.remove_stack(stack_name)
+        flash(f"Site '{stack_name}' successfully deleted", category="success")
+    except auto.ConcurrentUpdateError:
+        flash(f"Error: Site '{stack_name}' is currently updating", category="danger")
+    except Exception as exn:
+        flash(str(exn), category="danger")
+    
+    return redirect(url_for("sites.list_sites"))
